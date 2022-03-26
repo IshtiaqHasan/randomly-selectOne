b4c39f7c9abc1c0bd909360a327a71bd80a58c80
IshtiaqHasan <ishtiaqhasan1992@gmail.com>
2022-03-26 06:29:57 +0600

    header-style added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,8 +4,8 @@ import './Header.css'
 const Header = () => {
     return (
         <nav className='header'>
-            <h1>Headphones picker</h1>
-            <h3>We Choose The Best TWS For You</h3>
+            <h1 >Headphones picker</h1>
+            <p>We Choose The Best TWS For You</p>
         </nav>
     );
 };
